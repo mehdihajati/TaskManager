6741d82746e824077417f06a659843c6d3870bb6
Mehdi Hajati <mohamadmehdi.hajati@gmail.com>
2026-08-19 12:35:28 +0330

    feat: Get list of Projects of single user added to featues

--- src/TaskManager.Application/Features/Projects/Queries/GetProjectsForUser/GetProjectsForUserHandler.cs
+++ src/TaskManager.Application/Features/Projects/Queries/GetProjectsForUser/GetProjectsForUserHandler.cs
@@ -1,10 +1,30 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using MediatR;
+using TaskManager.Application.Common.Exceptions;
+using TaskManager.Application.Common.Interfaces;
+using TaskManager.Application.Features.Projects.DTOs;
+using TaskManager.Domain.Interfaces;
 
-namespace TaskManager.Application.Features.Projects.Queries.GetProjectsForUser
+namespace TaskManager.Application.Features.Projects.Queries.GetProjectsForUser;
+
+public class GetProjectsForUserHandler : IRequestHandler<GetProjectsForUserQuery, IEnumerable<ProjectDTO>>
 {
-    public class GetProjectsForUserHandler
+    private readonly IProjectRepository _projectRepository;
+    private readonly ICurrentUserService _currentUser;
+
+    public GetProjectsForUserHandler(IProjectRepository projectRepository, ICurrentUserService currentUser)
     {
+        _projectRepository = projectRepository;
+        _currentUser = currentUser;
+    }
+
+    public async Task<IEnumerable<ProjectDTO>> Handle(GetProjectsForUserQuery request, CancellationToken cancellationToken)
+    {
+        var currentUser = _currentUser.UserId;
+        if (currentUser == null)
+            throw new ForbiddenException("User is not authonticated!");
+        var usersProject = await _projectRepository.GetUserProjectsAsync(currentUser.Value);
+        var projectDTOs = usersProject.Select(x => new ProjectDTO(x.Id, x.OwnerId, x.Name, x.Description, x.Status, x.Deadline));
+        return projectDTOs;
+
     }
 }
--- src/TaskManager.Application/Features/Projects/Queries/GetProjectsForUser/GetProjectsForUserQuery.cs
+++ src/TaskManager.Application/Features/Projects/Queries/GetProjectsForUser/GetProjectsForUserQuery.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using MediatR;
+using TaskManager.Application.Features.Projects.DTOs;
 
-namespace TaskManager.Application.Features.Projects.Queries.GetProjectsForUser
-{
-    public class GetProjectsForUserQuery
-    {
-    }
-}
+namespace TaskManager.Application.Features.Projects.Queries.GetProjectsForUser;
+
+public record GetProjectsForUserQuery() : IRequest<IEnumerable<ProjectDTO>>;
