03e1bd4f214012672fcb8a319aadd797d1fb98b5
Mehdi Hajati <mohamadmehdi.hajati@gmail.com>
2026-09-20 12:29:19 +0330

    infrastructure dependencies registered successfully

--- src/TaskManager.Bootstrapper/DependencyInjection.cs
+++ src/TaskManager.Bootstrapper/DependencyInjection.cs
@@ -1,16 +1,16 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using TaskManager.Application;
+using TaskManager.Infrastructure;
 
 namespace TaskManager.Bootstrapper
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddAllServices(this IServiceCollection services)
+        public static IServiceCollection AddAllServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddApplication();
+            services.AddInfrastructure(configuration);
             return services;
         }
     }
